da140e8f31f11d56b9bab5dfea146915df00d3bf
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2022-07-07 23:19:17 +0200

    Add build

--- src/helpers/test/fetchVideoAndSet.ts
+++ src/helpers/test/fetchVideoAndSet.ts
@@ -1,6 +1,6 @@
 
 export const fetchVideoAndSet = (url : string, token : string, ref : React.RefObject<HTMLVideoElement>) => {
-    fetch(url, { method: 'GET', headers: { Authorization: `Bearer ${token}` }})
+    fetch(url, { method: 'GET'})
             .then(res => res.blob())
             .then((blob) => { if(ref.current !== null) ref.current.src = URL.createObjectURL(blob) })
             .catch(() => {})
